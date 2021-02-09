0d44002837f31e59eaeb2c59c2278c38a8962b15
Tanveer <johnnydalal11@gmail.com>
2021-02-09 21:39:08 +0000

    Link to practice with algebra, statistics and probability

---
+++ CS2004 Worksheet 3 - Mathematical Foundation/src/Lab_3.java
@@ -0,0 +1,10 @@
+
+public class Lab_3 {
+
+	/*
+	 * It's just an online test for testing your limits in Algebra and 
+	 * Statistics and Probability 
+	 * 
+	 * You can see the link in Word document shown in the repository
+	 * */
+}
